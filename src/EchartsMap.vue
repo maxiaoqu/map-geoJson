c095240f42e8ff67713ec0c2736ed45246e80921
maxiaoqu <maxiaoqu@163.com>
2020-04-30 17:36:37 +0800

    删除样式文件、增加echarts

---
+++ src/EchartsMap.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="body">
+        <div id="map"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EchartsMap",
+        data() {
+            return {
+                echartsMap: null
+            }
+        },
+        mounted() {
+
+        },
+        computed: {
+
+        },
+        methods: {}
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+    .body {
+        width 100%
+    }
+
+    #map {
+        width 100%
+        height 100vh
+    }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ const consoleInfo = require('./console');
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+    lintOnSave: false
 }
